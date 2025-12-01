85a7bde343f0ec1443b69543046af3d1a8c6a559
Tyler Richman <tyler.richman@erg.com>
2025-12-01 10:48:29 -0500

    Batch Update 2025-12-01

--- default_installation_file.py
+++ default_installation_file.py
@@ -1,4 +1,3 @@
-
 ## 1) Import Libraries
 
 import io
@@ -27,6 +26,14 @@ activate_venv_command = os.path.join(workspace_path, "python-env/Scripts/activat
 python_exe_location = os.path.join(workspace_path, "python-env/Scripts/python.exe")
 pip_exe_location = os.path.join(workspace_path, "python-env/Scripts/pip.exe")
 
+main_py_location = os.path.join(workspace_path, "dev/main.py")
+main_exe_folder_location = os.path.join(workspace_path, "exe")
+pyinstaller_exe_location = os.path.join(workspace_path, "python-env/Scripts/pyinstaller.exe")
+icon_location = os.path.join(workspace_path, "img/icon/main.png")
+
+output_exe_location = os.path.join(workspace_path, "exe/dist/main.exe")
+final_exe_location = os.path.join(workspace_path, "Tkinter-Application-Template_0_1_0.exe")
+
 ## 3) Download Repository to Workspace
 
 response = requests.get(code_zip_url)
@@ -47,6 +54,41 @@ with open(requirements_txt_path, "r") as f:
     for line in f:
         line = line.strip()   # remove newline
         subprocess.run([activate_venv_command, "&&", python_exe_location, pip_exe_location, "install", line], check=True)
+
+## 5) Create Enviroment for Application
+
+if os.path.exists(main_exe_folder_location):
+    shutil.rmtree(main_exe_folder_location)
+
+if not os.path.exists(main_exe_folder_location):
+    os.makedirs(main_exe_folder_location)
+
+## 6) Install Application
+
+try:
+    command = (
+        f'"{activate_venv_command}" && '
+        f'cd "{main_exe_folder_location}" && '
+        f'"{pyinstaller_exe_location}" --onefile --noconsole '
+        f'--icon="{icon_location}" '
+        f'--add-data=dev;dev '
+        f'--add-data=img;img '
+        #f'--add-data=txt;txt '
+        f'--add-data=default_installation_file.py;. '
+        f'--add-data=config.ini;. '
+        f'"{main_py_location}"'
+    )
+    subprocess.run(command, shell=True, check=True)
+
+except:
+    proc = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print(proc.stdout)
+    print(proc.stderr)
+
+shutil.copy(
+    output_exe_location,
+    final_exe_location
+    )
 
 #NOTES
 #1) Repository needs to be public for this to work.
